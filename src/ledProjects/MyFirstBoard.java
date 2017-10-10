283e4d8b87a4c88af3429791395c91ed36dce0f9
Piet1848 <piet.leyendecker@gmail.com>
2017-10-10 18:35:38 +0200

    inital build up

---
+++ src/ledProjects/MyFirstBoard.java
@@ -0,0 +1,16 @@
+package ledProjects;
+
+import ledControl.BoardController;
+
+public class MyFirstBoard {
+	
+	static BoardController controller = BoardController.getBoardController();
+
+	public static void main(String[] args) {
+		controller.setColor(5, 5, 127, 127, 127);
+		controller.updateLedStripe();
+
+		
+	}
+
+}
